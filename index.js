fee1d47ec95e8df0141ef4771f603c02c1db1994
uvcanaditya <uvcan.aditya@gmail.com>
2023-09-19 02:21:48 +0530

    setting up Mongo DB

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const port=8000;
 const app=express();
 const expressLayouts=require('express-ejs-layouts');
+const db=require('./config/mongoose');
 
 
 //telling the use the static file from assets folder 
