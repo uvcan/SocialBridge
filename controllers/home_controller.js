37b2624bcc6a80b966056b5664a2ba39a36713c5
uvcanaditya <uvcan.aditya@gmail.com>
2023-09-21 03:29:50 +0530

    Populate the user who has posted and checkAuthenticated

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,19 @@
-module.exports.home=function(req,res){
+const Post = require("../models/post");
+
+module.exports.home= async function(req,res){
+     
     // console.log(req.cookies);
     // res.cookie('user_id',25);
-    return res.render('home',{
-        title:"Home"
-    });
+    try{
+        const posts = await Post.find({}).populate('user').exec();
+
+        return res.render('home', {
+            title: "SocialBridge Home",
+            post: posts
+        });
+        
+    }catch(err){
+        console.error('Error in fetching posts', err);
+    }
+  
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -14,3 +14,4 @@ module.exports.create=async function(req,res){
         return; 
     }
 }
+
